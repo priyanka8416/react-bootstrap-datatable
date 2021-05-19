0866e38fd2a0a589e5b8a3947d5120edf1f28933
Priyanka Singh <84117066+priyanka8416@users.noreply.github.com>
2021-05-19 21:17:17 +0530

    datatable page

---
+++ src/pages/DataTable/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState, useMemo } from "react";
+import TableHeader from "../../components";
+
+const DataTable = () => {
+
+  const headers = [
+    { name: 'No#', field: 'id', sortable: false },
+    { name: 'Name', field: 'name', sortable: true },
+    { name: 'Email', field: 'email', sortable: true },
+    { name: 'Comment', field: 'body', sortable: false }
+  ]
+
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      fetch("https://jsonplaceholder.typicode.com/comments")
+        .then(response => response.json())
+        .then(json => {
+          setComments(json);
+        });
+    };
+    getData();
+
+  }, []);
+
+  const commentsData = useMemo(() => {
+    let computedComments = comments;
+    return computedComments
+  }, [comments]);
+
+  return (
+    <table className="table table-striped">
+      <TableHeader headers={headers} />
+      <tbody>
+        {commentsData.map(comment => (
+          <tr key={comment.id}>
+            <th scope="row" key={comment.id}>
+              {comment.id}
+            </th>
+            <td>{comment.name}</td>
+            <td>{comment.email}</td>
+            <td>{comment.body}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default DataTable;
