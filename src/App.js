b8fc7df2183e9a3312604d4463ba4eef4d2161e6
Priyanka Singh <84117066+priyanka8416@users.noreply.github.com>
2021-05-20 16:03:08 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import DataTable from './pages/DataTable'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import DataTable from './pages/DataTable';
+import Home from './pages/Home';
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
-    <DataTable />
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/datatable">
+          <DataTable />
+        </Route>
+        <Route path="*"><div>Page Not Found</div></Route>
+      </Switch>
+    </Router>
   );
 }
 
