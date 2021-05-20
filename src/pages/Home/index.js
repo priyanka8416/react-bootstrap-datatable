667dbff0a57cbd3a714bb11a747eedf44d80dd53
Priyanka Singh <84117066+priyanka8416@users.noreply.github.com>
2021-05-20 18:26:52 +0530

    added noreferrer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,7 +9,7 @@ const Home = () => {
         <h3 className="pb-2 text-center">React component links &amp; its Github repository<img className="arrow" src={arrow} alt="arrow" /></h3>
       </div>
       <div className="d-flex justify-content-center">
-        <a class="btn btn-primary" href="https://github.com/priyanka8416/react-deposit" role="button" target="_blank">Github Repository</a>
+        <a class="btn btn-primary" href="https://github.com/priyanka8416/react-deposit" role="button" target="_blank" rel="noreferrer">Github Repository</a>
       </div>
     </div>
   )
